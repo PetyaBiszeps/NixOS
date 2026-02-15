1470ff66aa3fe66c1fc46a87de204f9a0faa9dd8
YP00 <PersonalYP00@gmail.com>
2026-02-15 22:20:40 +0200

    commit changes.

--- system/configuration.nix
+++ system/configuration.nix
@@ -6,17 +6,17 @@
   imports = [
     ./hardware-configuration.nix
 
-    ../modules/audio.nix
-    ../modules/bootLoader.nix
-    ../modules/dev.nix
-    ../modules/fileManager.nix
-    ../modules/fonts.nix
-    ../modules/gui.nix
-    ../modules/locales.nix
-    ../modules/network.nix
-    ../modules/packageManager.nix
-    ../modules/packages.nix
-    ../modules/user.nix
+    ./modules/audio.nix
+    ./modules/bootLoader.nix
+    ./modules/dev.nix
+    ./modules/fileManager.nix
+    ./modules/fonts.nix
+    ./modules/gui.nix
+    ./modules/locales.nix
+    ./modules/network.nix
+    ./modules/packageManager.nix
+    ./modules/packages.nix
+    ./modules/user.nix
   ];
 
   # NixOS current version (Don't remove that unless you know what you do)
