83c9fc6b933aedd1f40f028f6f64195f557cc37b
YP00 <PersonalYP00@gmail.com>
2026-02-18 20:37:50 +0200

    feat: update hardware-configuration.nix

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -24,7 +24,9 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/98c47445-6bf9-40ff-aa33-53f9a68e2f20"; }
+    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
