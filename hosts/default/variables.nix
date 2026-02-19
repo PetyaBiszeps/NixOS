0a6fd934ca754db682d6f12c3d05d2ddbdd86716
YP00 <personalYP00@icloud.com>
2026-02-19 02:41:02 +0200

    feat: variables.nix setup

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -1,5 +1,36 @@
-#
-#
-#
+# Default host variables
+# Vocabulary for important constants
+# Feel free to add, remove and edit anything here as you wish
 
-{}
+{
+  # System
+  timeZone = "Europe/Kyiv";
+  keyboardLayout = "us";
+  monitorSettings = "monitor=,preferred,auto,1";
+
+  # Git
+  gitUsername = "user";
+  gitEmail = "user@example.com";
+
+  # Software
+  browser = "zen";
+  terminal = "ghostty";
+
+  # Enable NFS
+  enableNFS = true;
+
+  # Enable printing support
+  printEnable = true;
+
+  # Enable Nautilus GUI file manager
+  nautilusEnable = true;
+
+  # Enable gaming support (controllers, gamescope, protonup-qt)
+  gamingSupportEnable = true;
+
+  # Shell choice
+  defaultShell = "zsh";
+
+  # Quickshell Choice
+  desktopShell = "noctalia";
+}
