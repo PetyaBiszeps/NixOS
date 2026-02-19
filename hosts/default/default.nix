2ab958dafc16fada862b62e0a0f29ea03df461dc
YP00 <personalYP00@icloud.com>
2026-02-19 02:16:59 +0200

    feat: host default.nix setup

--- hosts/default/default.nix
+++ hosts/default/default.nix
@@ -1,5 +1,19 @@
-#
-#
-#
+# Nix default host configuration file
+# Detects user hardware configuration to make proper build
+# Enables display manager, sync GUI (Niri) session to be available to display manager
 
-{}: {}
+{ pkgs, ... }: {
+  imports = [
+    ./hardware.nix
+    ./host-packages.nix
+  ];
+
+  # Display manager
+  services.displayManager.ly.enable = true;
+
+  # Keep niri available at system level
+  programs.niri.package = pkgs.niri;
+
+  # Ensure niri session is available to display manager
+  services.displayManager.sessionPackages = [ pkgs.niri ];
+}
