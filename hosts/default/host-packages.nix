e597c98bba6b4dd5925139302844f0d40115fc69
YP00 <personalYP00@icloud.com>
2026-02-19 02:28:05 +0200

    feat: host-packages.nix setup

--- hosts/default/host-packages.nix
+++ hosts/default/host-packages.nix
@@ -1,5 +1,9 @@
-#
-#
-#
+# Nix host packages configuration
+# Provides file to install specific packages
+# Feel free to add, remove and modify anything there
 
-{}: {}
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    # Add host-specific packages here
+  ];
+}
