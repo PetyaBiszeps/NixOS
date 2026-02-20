4cdc9a6fe972887162600a49c1fcadbaa6841b86
YP00 <personalYP00@icloud.com>
2026-02-20 04:22:49 +0200

    feat: nixos-rebuild-all alias adjustment

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -7,8 +7,9 @@
     enable = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
     shellAliases = {
-      nixos-rebuild-all = "sudo nixos-rebuild switch --flake \"path:.#nixos\" --no-update-lock-file && sudo nix-collect-garbage -d";
+      nixos-rebuild-all = "sudo nixos-rebuild switch --flake \"path:.#nixos\" -- --no-write-lock-file && sudo nix-collect-garbage -d";
     };
   };
 }
