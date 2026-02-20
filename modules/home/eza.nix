e6bb74a8a6b42d805087fb6e78843937c064d425
YP00 <personalYP00@icloud.com>
2026-02-20 03:51:29 +0200

    bug: fix eza

--- modules/home/eza.nix
+++ modules/home/eza.nix
@@ -2,9 +2,12 @@
 # Provides eza configuration
 # Feel free to add, remove and modify anything here
 
-{ ... }: {
+{ variables, ... }:
+  let defaultShell = variables.defaultShell or "zsh";
+in {
   programs.eza = {
     enable = true;
-    enableAliases = true;
+    enableZshIntegration = defaultShell == "zsh";
+    enableFishIntegration = defaultShell == "fish";
   };
 }
