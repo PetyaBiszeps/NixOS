633b2491c6755a07b9c986706a126cc60e8445bd
YP00 <personalYP00@icloud.com>
2026-02-21 02:25:22 +0200

    bug: fix ly variable from array to string

--- modules/core/ly.nix
+++ modules/core/ly.nix
@@ -17,44 +17,11 @@
       xsessions = null;
       animation = "matrix";
       save_file = "/var/lib/ly/save";
+      hide_users = "nixbld1,nixbld2,nixbld3,nixbld4,nixbld5,nixbld6,nixbld7,nixbld8,nixbld9,nixbld10,nixbld11,nixbld12,nixbld13,nixbld14,nixbld15,nixbld16,nixbld17,nixbld18,nixbld19,nixbld20,nixbld21,nixbld22,nixbld23,nixbld24,nixbld25,nixbld26,nixbld27,nixbld28,nixbld29,nixbld30,nixbld31,nixbld32";
       default_input = "password";
       # hide_borders = true;
       hide_system_users = true;
       # min_refresh_delta = 5;
-      hide_users = [
-        "nixbld1"
-        "nixbld2"
-        "nixbld3"
-        "nixbld4"
-        "nixbld5"
-        "nixbld6"
-        "nixbld7"
-        "nixbld8"
-        "nixbld9"
-        "nixbld10"
-        "nixbld11"
-        "nixbld12"
-        "nixbld13"
-        "nixbld14"
-        "nixbld15"
-        "nixbld16"
-        "nixbld17"
-        "nixbld18"
-        "nixbld19"
-        "nixbld20"
-        "nixbld21"
-        "nixbld22"
-        "nixbld23"
-        "nixbld24"
-        "nixbld25"
-        "nixbld26"
-        "nixbld27"
-        "nixbld28"
-        "nixbld29"
-        "nixbld30"
-        "nixbld31"
-        "nixbld32"
-      ];
     };
   };
 
