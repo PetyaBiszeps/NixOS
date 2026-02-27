6fe02c0c7ffd0808c33859a4edefb463b2edc3a5
YP00 <personalYP00@icloud.com>
2026-02-27 15:58:58 +0200

    feat: nvf decompose

---
+++ modules/home/nvf/lsp.nix
@@ -0,0 +1,28 @@
+# NVF LSP configuration
+# Provides language server setup
+# Feel free to add, remove and modify anything here
+
+{ ... }: {
+  programs.nvf.settings.vim.lsp = {
+    enable = true;
+    lspconfig.enable = true;
+
+    servers = {
+      nixd = {
+        enable = true;
+
+        settings = {
+          nixd = {
+            autoArchive = true;
+          };
+        };
+      };
+
+      ts_ls.enable = true;
+      gopls.enable = true;
+      vue_ls.enable = true;
+      pyright.enable = true;
+      bash_ls.enable = true;
+    };
+  };
+}
