3d2dfeed0e736d6b570102f44361030b6611c58f
YP00 <personalYP00@icloud.com>
2026-02-27 02:40:14 +0200

    fix: yazi errors part III

--- modules/home/yazi/theme.nix
+++ modules/home/yazi/theme.nix
@@ -2,4 +2,15 @@
 # Provides Yazi theming config
 # Feel free to add, remove and modify anything here
 
-{ ... }: {}
+{
+  manager = {
+    cwd = {
+      fg = "blue";
+    };
+
+    hovered = {
+      fg = "black";
+      bg = "green";
+    };
+  };
+}
