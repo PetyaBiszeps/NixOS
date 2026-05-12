7e559b4af02765aa9508c8d2d514817c796b23a9
YP00 <PersonalYP00@gmail.com>
2026-05-13 01:34:05 +0300

    feat: add Cliphist support

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -60,4 +60,22 @@ in {
 
     Install.WantedBy = ["graphical-session.target"];
   };
+
+  # Cliphist
+  systemd.user.services.cliphist = {
+    Unit = {
+      Description = "Clipboard history";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store";
+      Restart = "on-failure";
+    };
+
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
 }
