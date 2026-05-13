85b75ab1b450f14cedfece124af23d1bd132515c
YP00 <PersonalYP00@gmail.com>
2026-05-13 03:12:53 +0300

    feat: update flake

--- modules/core/display/sessions/default.nix
+++ modules/core/display/sessions/default.nix
@@ -2,11 +2,8 @@
 # Provides available graphical session modules
 # Be careful when adding or removing modules, as it may affect desktop startup
 
-{ config, lib, ... }:
-  let defaultSession = config.variables.defaultSession or "niri";
-in {
-  imports = 
-    lib.optionals (defaultSession == "niri") [
-      ./niri.nix
-    ];
+{ ... }: {
+  imports = [
+    ./niri.nix
+  ];
 }
--- modules/core/display/sessions/niri.nix
+++ modules/core/display/sessions/niri.nix
@@ -2,16 +2,18 @@
 # Provides Niri graphical session support
 # Feel free to add, remove and modify anything here
 
-{ inputs, pkgs, ... }: {
+{ config, lib, inputs, pkgs, ... }:
+  let defaultSession = config.variables.defaultSession or "niri";
+in {
   imports = [ inputs.niri.nixosModules.niri ];
 
-  nixpkgs.overlays = [
-    inputs.niri.overlays.niri
-  ];
+  config = lib.mkIf (defaultSession == "niri") {
+    nixpkgs.overlays = [ inputs.niri.overlays.niri ];
 
-  programs.niri = {
-    enable = true;
-    package = pkgs.niri-unstable;
+    programs.niri = {
+      enable = true;
+      package = pkgs.niri-unstable;
+    };
+    services.displayManager.defaultSession = "niri";
   };
-  services.displayManager.defaultSession = "niri";
 }
