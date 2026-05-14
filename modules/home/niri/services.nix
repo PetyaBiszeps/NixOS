c2b3c409ddb2540f9430e36fc68b7224881f3ed0
YP00 <PersonalYP00@gmail.com>
2026-05-14 21:59:38 +0300

    fix: wayland-satelite service start removed due Niri autostart

--- modules/home/niri/services.nix
+++ modules/home/niri/services.nix
@@ -3,26 +3,6 @@
 # Be careful modifying this file, as it may affect the Niri session
 
 { pkgs, ... }: {
-  # Xwayland Satellite support for Niri
-  systemd.user.services.xwayland-satellite = {
-    Unit = {
-      Description = "Xwayland outside Wayland";
-      BindsTo = "graphical-session.target";
-      After = "graphical-session.target";
-      ConditionEnvironment = "WAYLAND_DISPLAY";
-    };
-
-    Service = {
-      Type = "notify";
-      NotifyAccess = "all";
-      Restart = "on-failure";
-      StandardOutput = "journal";
-      ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite";
-    };
-
-    Install.WantedBy = ["graphical-session.target"];
-  };
-
   # Cliphist
   systemd.user.services.cliphist = {
     Unit = {
