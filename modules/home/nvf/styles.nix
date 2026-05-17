a526f5d1365088e04d28cdd4ab495969ac902ee5
YP00 <PersonalYP00@gmail.com>
2026-05-18 02:38:54 +0300

    fix: deprecated vim options

--- modules/home/nvf/styles.nix
+++ modules/home/nvf/styles.nix
@@ -3,21 +3,17 @@
 
 { lib, ... }: {
   programs.nvf.settings.vim.luaConfigRC.ui = lib.mkAfter ''
+    -- Global floating window border for Neovim
+    pcall(function()
+      vim.o.winborder = "rounded"
+    end)
+
+    -- Diagnostic floating windows
     vim.diagnostic.config({
       float = {
         border = "rounded",
         source = "if_many",
       },
     })
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-      vim.lsp.handlers.hover,
-      { border = "rounded" }
-    )
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-      vim.lsp.handlers.signature_help,
-      { border = "rounded" }
-    )
   '';
 }
