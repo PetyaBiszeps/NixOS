cc1e32ae5aa3fded55a687d83f061390627cf386
YP00 <PersonalYP00@gmail.com>
2026-05-17 19:05:03 +0300

    feat: add blink-cmp instead of nvim-cmp

--- modules/home/nvf/plugins/completion.nix
+++ modules/home/nvf/plugins/completion.nix
@@ -2,7 +2,31 @@
 # Provides completion engine for LSP, snippets, buffers, paths, and commands
 
 { ... }: {
-  programs.nvf.settings.vim.autocomplete.nvim-cmp = {
+  programs.nvf.settings.vim.autocomplete.blink-cmp = {
     enable = true;
+
+    setupOpts = {
+      keymap.preset = "super-tab";
+
+      completion = {
+        documentation = {
+          auto_show = true;
+          auto_show_delay_ms = 200;
+        };
+        menu.auto_show = true;
+      };
+
+      sources.default = [
+        "lsp"
+        "path"
+        "buffer"
+        "snippets"
+      ];
+
+      fuzzy = {
+        implementation = "prefer_rust";
+        prebuilt_binaries.download = false;
+      };
+    };
   };
 }
