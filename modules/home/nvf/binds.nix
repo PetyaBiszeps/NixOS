a5ed180d1cca7e2ca7f6a629a95b66851a256955
YP00 <PersonalYP00@gmail.com>
2026-05-18 01:56:11 +0300

    feat: add diagnostic binds

--- modules/home/nvf/binds.nix
+++ modules/home/nvf/binds.nix
@@ -14,20 +14,40 @@
         desc = "Open parent directory";
       };
 
-      "<leader>e" = {
-        action = "<cmd>Oil<CR>";
-        desc = "Open Oil";
-      };
+      # "<leader>e" = {
+      #   action = "<cmd>Oil<CR>";
+      #   desc = "Open Oil";
+      # };
 
-      "<leader>E" = {
+      "<leader>e" = {
         action = "<cmd>Oil --float<CR>";
         desc = "Open Oil float";
       };
+
+      "<leader>d" = {
+        action = "<cmd>lua vim.diagnostic.open_float()<CR>";
+        desc = "Show diagnostic";
+      };
+
+      "]d" = {
+        action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
+        desc = "Next diagnostic";
+      };
+
+      "[d" = {
+        action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
+        desc = "Previous diagnostic";
+      };
+
+      "<leader>q" = {
+        action = "<cmd>lua vim.diagnostic.setloclist()<CR>";
+        desc = "Diagnostics list";
+      };
     };
-    
+
     binds.whichKey = {
       enable = true;
-  
+
       setupOpts = {
         preset = "modern";
         win.border = "rounded";
