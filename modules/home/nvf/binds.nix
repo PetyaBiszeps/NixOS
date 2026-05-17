478e4d785fdabc6620e25d841d4a1d443efb7cad
YP00 <PersonalYP00@gmail.com>
2026-05-18 02:52:32 +0300

    feat: add various keymaps

--- modules/home/nvf/binds.nix
+++ modules/home/nvf/binds.nix
@@ -8,40 +8,74 @@
       maplocalleader = " ";
     };
 
-    maps.normal = {
-      "-" = {
-        action = "<cmd>Oil<CR>";
-        desc = "Open parent directory";
+    maps = {
+      normal = {
+        "-" = {
+          action = "<cmd>Oil<CR>";
+          desc = "Open parent directory";
+        };
+
+        "<leader>e" = {
+          action = "<cmd>Oil --float<CR>";
+          desc = "Open Oil float";
+        };
+
+        "<leader>dd" = {
+          action = "<cmd>lua vim.diagnostic.open_float()<CR>";
+          desc = "Show diagnostic";
+        };
+
+        "<leader>dq" = {
+          action = "<cmd>lua vim.diagnostic.setloclist()<CR>";
+          desc = "Diagnostics list";
+        };
+
+        "]d" = {
+          action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
+          desc = "Next diagnostic";
+        };
+
+        "[d" = {
+          action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
+          desc = "Previous diagnostic";
+        };
+
+        "<C-d>" = {
+          action = "<C-d>zz";
+          desc = "Scroll down and center";
+        };
+
+        "<C-u>" = {
+          action = "<C-u>zz";
+          desc = "Scroll up and center";
+        };
+
+        "<leader>y" = {
+          action = ''"+y'';
+          desc = "Yank to system clipboard";
+        };
+
+        "<leader>Y" = {
+          action = ''"+yy'';
+          desc = "Yank line to system clipboard";
+        };
+
+        "<leader>D" = {
+          action = ''"+d'';
+          desc = "Delete to system clipboard";
+        };
       };
 
-      # "<leader>e" = {
-      #   action = "<cmd>Oil<CR>";
-      #   desc = "Open Oil";
-      # };
+      visual = {
+        "<leader>y" = {
+          action = ''"+y'';
+          desc = "Yank to system clipboard";
+        };
 
-      "<leader>e" = {
-        action = "<cmd>Oil --float<CR>";
-        desc = "Open Oil float";
-      };
-
-      "<leader>d" = {
-        action = "<cmd>lua vim.diagnostic.open_float()<CR>";
-        desc = "Show diagnostic";
-      };
-
-      "]d" = {
-        action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
-        desc = "Next diagnostic";
-      };
-
-      "[d" = {
-        action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
-        desc = "Previous diagnostic";
-      };
-
-      "<leader>q" = {
-        action = "<cmd>lua vim.diagnostic.setloclist()<CR>";
-        desc = "Diagnostics list";
+        "<leader>D" = {
+          action = ''"+d'';
+          desc = "Delete to system clipboard";
+        };
       };
     };
 
