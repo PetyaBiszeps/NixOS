c79636b943c244b0d4a4b9187d44870721978f45
YP00 <PersonalYP00@gmail.com>
2026-06-27 14:54:37 +0300

    feat: vulkan adjustments, update flake

--- modules/core/nix/ld.nix
+++ modules/core/nix/ld.nix
@@ -1,12 +1,14 @@
 # Nix ld module
 # Provides nix-ld compatibility support
 
-{ ... }: {
+{ pkgs, ... }: {
   programs.nix-ld = {
     enable = true;
 
-    # libraries = with pkgs; [
-    #   
-    # ];
+    libraries = with pkgs; [
+      libGL
+      libGLU
+      libGLX
+    ];
   };
 }
